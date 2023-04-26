d1568e7a994f859c9b6e17dc6084eeebce36a5a1
ghockin <79321882+ghockin@users.noreply.github.com>
2023-04-26 20:49:12 +0100

    add product

---
+++ components/Layout.js
@@ -0,0 +1,31 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import Nav from "@/components/Nav";
+
+
+export default function Layout({children}) {
+  const { data: session } = useSession()
+
+
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-cnenter">
+        <div className="text-center w-full">
+          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with google</button>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="bg-blue-900 w-screen h-screen flex">
+      <Nav />
+
+      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
+        {/* Logged In {session.user.email} */}
+        {children}
+      </div>
+
+
+    </div>
+  )
+}

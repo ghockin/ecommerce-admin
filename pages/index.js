ae07e3384a03e76549af2629653fe37b2243ef3e
ghockin <79321882+ghockin@users.noreply.github.com>
2023-04-26 12:44:49 +0100

    connected to mongo db

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
+  const { data: session } = useSession()
+
+
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-cnenter">
+        <div className="text-center w-full">
+        <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with google</button>
+        </div>
+      </div>
+    )
+  }
+
   return (
-      <div className="{bg-red-500}">test </div>
+    <div>
+      Logged In {session.user.email}
+    </div>
   )
 }
